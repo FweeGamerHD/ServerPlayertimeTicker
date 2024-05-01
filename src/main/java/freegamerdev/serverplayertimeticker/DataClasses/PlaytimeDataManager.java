6a14cbac70c833660247f3e213140e7d79e73092
FweeGamerHD <freegamerhd12@gmail.com>
2024-05-01 23:00:12 +0200

    moved data to config folder for the mod (also used later)

--- src/main/java/freegamerdev/serverplayertimeticker/DataClasses/PlaytimeDataManager.java
+++ src/main/java/freegamerdev/serverplayertimeticker/DataClasses/PlaytimeDataManager.java
@@ -13,19 +13,23 @@ import java.nio.file.Paths;
 public class PlaytimeDataManager {
     private static final Gson gson = new GsonBuilder().setPrettyPrinting().create();
     private static final Type PLAYTIME_DATA_TYPE = new TypeToken<PlaytimeData>() {}.getType();
-    private static final Path PLAYTIME_DATA_FILE = Paths.get("playtime_data.json");
+    private static final Path PLAYTIME_DATA_FILE = Paths.get("config/serverplayertime/playtime_data.json");
 
     public static PlaytimeData loadPlaytimeData() throws IOException {
         if (Files.exists(PLAYTIME_DATA_FILE)) {
             String jsonData = Files.readString(PLAYTIME_DATA_FILE);
             return gson.fromJson(jsonData, PLAYTIME_DATA_TYPE);
         } else {
-            return new PlaytimeData();
+            // If the file doesn't exist, create it and initialize with default data
+            PlaytimeData defaultData = new PlaytimeData();
+            savePlaytimeData(defaultData); // Save default data to file
+            return defaultData;
         }
     }
 
     public static void savePlaytimeData(PlaytimeData playtimeData) throws IOException {
         String jsonData = gson.toJson(playtimeData, PLAYTIME_DATA_TYPE);
+        Files.createDirectories(PLAYTIME_DATA_FILE.getParent()); // Create directories if they don't exist
         Files.writeString(PLAYTIME_DATA_FILE, jsonData);
     }
 }
